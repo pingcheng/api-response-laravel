600154faf9b9c4946cf0da35f79378e4ccb10273
Ping Cheng <ping.che@hotmail.com>
2019-05-12 23:20:40 +1000

    add more testing cases

--- tests/TestJsonResponse.php
+++ tests/TestJsonResponse.php
@@ -51,4 +51,39 @@ class TestJsonResponse extends TestCase
             $response->stringify()
         );
     }
+
+    public function testGetDefaultHeaders(): void
+    {
+        $response = new JsonResponse();
+        $default_headers = $response->getHeaders();
+        $this->assertArrayHasKey('Content-Type', $default_headers);
+        $this->assertEquals('application/json', $default_headers['Content-Type']);
+    }
+
+    public function testSetHeader(): void
+    {
+        $response = new JsonResponse();
+        $response->setHeader('X-Custom-Header', 'Testing');
+
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('X-Custom-Header', $headers);
+        $this->assertEquals('Testing', $headers['X-Custom-Header']);
+    }
+
+    public function testSetHeaders(): void
+    {
+        $extra_headers = [
+            'h1' => 'v1',
+            'h2' => 'v2',
+        ];
+        $response = new JsonResponse();
+        $default_headers = $response->getHeaders();
+
+        $response->setHeaders($extra_headers);
+        $new_headers = $response->getHeaders();
+
+        $merged_headers = array_merge($default_headers, $extra_headers);
+        $this->assertEquals($merged_headers, $new_headers);
+
+    }
 }
